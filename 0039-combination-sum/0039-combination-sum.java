d3d8ab26ec649ec05713d1a0edee51d1fe66ba0e
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-07-18 22:20:32 +0530

    Time: 1 ms (99.9%), Space: 44.6 MB (42.35%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void find(int arr[], int target, int i, List<List<Integer>> ans, List<Integer> ds){
+        if(i == arr.length){
+            if(target == 0) {
+                ans.add(new ArrayList<>(ds));
+            }
+            return;
+        }
+
+        if(arr[i] <= target){
+            ds.add(arr[i]);
+            find(arr, target-arr[i], i, ans, ds);
+            ds.remove(ds.size()-1);
+        }
+        find(arr, target, i+1, ans, ds);
+    }
+
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        find(arr, target, 0, ans, new ArrayList<>());
+        return ans;
+    }
+}
