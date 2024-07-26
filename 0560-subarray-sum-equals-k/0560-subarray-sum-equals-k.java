dd82bfc371d66450adb2f9c79a67345ba6216462
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-07-26 22:17:20 +0530

    Time: 25 ms (47.97%), Space: 46.3 MB (61.58%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int subarraySum(int[] arr, int k) {
-        int n = arr.length;
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
         Map <Integer, Integer> mpp = new HashMap<>();
         int preSum = 0, cnt = 0;
 
         mpp.put(0, 1);
         for (int i = 0; i < n; i++) {
-            preSum += arr[i];
+            preSum += nums[i];
             int remove = preSum - k;
             cnt += mpp.getOrDefault(remove, 0);
             mpp.put(preSum, mpp.getOrDefault(preSum, 0) + 1);
