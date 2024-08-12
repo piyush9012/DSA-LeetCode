09026540224ed26612fb2fe924168312c87c3d86
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-08-13 00:08:34 +0530

    Time: 1 ms (98.87%), Space: 41.2 MB (80.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -5,12 +5,12 @@ class Solution {
             if (it == '(' || it == '[' || it == '{')
                 st.push(it);
             else {
-                if(st.isEmpty()) return false;
+                if(st.isEmpty()) return false;  // check if there is only one opening bracket in the string
                 char ch = st.pop(); 
                 if((it == ')' && ch == '(') ||  (it == ']' && ch == '[') || (it == '}' && ch == '{')) continue;
                 else return false;
             }
         }
-        return st.isEmpty();
+        return st.isEmpty();   // empty string is also an answer, therefore return true if string is empty
     }
 }
