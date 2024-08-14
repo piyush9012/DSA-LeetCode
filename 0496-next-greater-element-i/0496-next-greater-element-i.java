38e2a62816c3090240ed475784a9ca02617e4eb2
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-08-14 18:35:36 +0530

    Time: 3 ms (92.23%), Space: 44.5 MB (10.68%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,17 +1,36 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        // int n1 = nums1.length;
+        // int n2 = nums2.length;
+        // Stack<Integer> st = new Stack<>();
+        // Map <Integer, Integer> mpp = new HashMap<>();
+        // for(int i = n2-1; i>= 0; i--){
+        //     if(!st.isEmpty() && st.peek() <= nums2[i]){
+        //         st.pop();
+        //     }
+        //     mpp.put(nums2[i], st.isEmpty()? -1: st.peek());
+        //     st.push(nums2[i]);
+        // }
+
+        // int ans[] = new int[n1];
+        // for(int i = 0; i<n1; i++){
+        //     ans[i] = mpp.get(nums1[i]); 
+        // }
+        // return ans;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
         Stack <Integer> st = new Stack<>();
         Map<Integer, Integer> mpp = new HashMap<>();
-        for(int i = nums2.length-1; i>= 0; i--){
-            while(st.isEmpty() == false && st.peek() <= nums2[i]){
+        for(int i = n2-1; i>= 0; i--){
+            while(!st.isEmpty() && st.peek() <= nums2[i]){
                 st.pop();
             }
             mpp.put(nums2[i], st.isEmpty()? -1: st.peek());
             st.push(nums2[i]);
         }
 
-        int ans[] = new int[nums1.length];
-        for(int i=0; i<nums1.length; i++){
+        int ans[] = new int[n1];
+        for(int i=0; i<n1; i++){
             ans[i] = mpp.get(nums1[i]);
         }
         return ans;
