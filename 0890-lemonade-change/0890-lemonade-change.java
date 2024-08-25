c12586cbc4400e1f7a457726a2a4b8efe5f2e50e
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-08-25 13:02:41 +0530

    Time: 2 ms (89.57%), Space: 55.8 MB (52.09%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -3,20 +3,21 @@ class Solution {
         int n = bills.length;
         int five = 0, ten = 0;
         for(int i = 0; i<n; i++){
-            if(i == 0 && bills[i] > 5) return false;
-
             if(bills[i] == 5) five++;
+
             else if(bills[i] == 10){
-                ten++;
-                if(five >= 1) five--;
+                if(five > 0){
+                    five--;
+                    ten++;
+                }
                 else return false;
             }
             else{
-                if(ten >= 1 && five >= 1){
-                    ten--;
+                if(five > 0 && ten > 0){
                     five--;
+                    ten--;
                 }
-                else if(ten == 0 && five >= 3){
+                else if(five >= 3){
                     five -= 3;
                 }
                 else return false;
